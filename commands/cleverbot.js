18ad875a27894fa87e6473de503b8334f1d6d62f
msd-11 <msd0xB@pm.me>
2022-09-17 13:19:06 +0200

    Add cleverbot command

---
+++ commands/cleverbot.js
@@ -0,0 +1,61 @@
+const alexa = require('alexa-bot-api-v4');
+const ai = new alexa();
+const cleverbot = require('cleverbot-free');
+
+// Array : [[id, Date]]
+var userHistory = [];
+
+// Array : [[message, message2, message3]]
+var history = [];
+
+const checkUser = (id) => {
+  var returnIndex = -1;
+  var currDate = new Date().getTime();
+
+  //  Vérifie si l'utilisateur a déjà une conversation avec le bot et qu'elle date de moins de 10 minutes
+  //  Renvoie son index si il existe
+  userHistory.map((value, index) => {
+    if (value[0] === id) {
+      // Moins de 10 minutes
+      if ((currDate - value[1]) / 1000 / 60 < 10) {
+        // Mets à jour son timer
+        userHistory[index][1] = new Date().getTime();
+        returnIndex = index;
+      } else {
+        // User existe mais conversation de + de 10 minutes
+        // On l'enlève de l'historique
+        userHistory.splice(index, 1);
+        history.splice(index, 1);
+      }
+    }
+  });
+
+  return returnIndex;
+};
+
+//Envoi un message à cleverbot et renvoie sa réponse
+module.exports = (message) => {
+  var messageString = message.content.split('&c ')[1];
+
+  var userIndex = checkUser(message.author.id);
+
+  // Premier message avec le bot
+  if (userIndex === -1) {
+    cleverbot(messageString).then((response) => {
+      // Création de l'user et début de conv dans l'historique
+      userHistory.push([message.author.id, new Date().getTime()]);
+      history.push([messageString, response]);
+
+      message.reply(response);
+    });
+  }
+  // Déjà en conversation avec le bot
+  else {
+    cleverbot(messageString, history[userIndex]).then((response) => {
+      // Mets à jour l'historique de conv
+      history[userIndex].push(messageString);
+      history[userIndex].push(response);
+      message.reply(response);
+    });
+  }
+};
--- index.js
+++ index.js
@@ -1,95 +1,97 @@
-const { Client, GatewayIntentBits, AttachmentBuilder } = require("discord.js");
-require("dotenv").config();
+const { Client, GatewayIntentBits, AttachmentBuilder } = require('discord.js');
+require('dotenv').config();
 
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent
-    ]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
 });
 
 //Préfixe des commandes
-const prefixCmd = '&';
+const prefixCmd = '²';
 
 //Lorsque le bot est prêt
-client.on("ready", () => {
-    //Message de status
-    client.user.setActivity(prefixCmd + 'help');
-    console.log("Bot ready");
+client.on('ready', () => {
+  //Message de status
+  client.user.setActivity(prefixCmd + 'help');
+  console.log('Bot ready');
 });
 
 //Lorsqu'un message est envoyé
-client.on("messageCreate", message => {
+client.on('messageCreate', (message) => {
+  //Si le message vient d'un bot, on ne traite pas
+  if (message.author.bot) return;
 
-    //Si le message vient d'un bot, on ne traite pas
-    if (message.author.bot) return;
+  //Si c'est un rapport Epic Report
+  if (message.content.startsWith('[Epic Report]')) {
+    require('./commands/epicreport.js')(message);
+    return;
+  }
 
-    //Si c'est un rapport Epic Report
-    if (message.content.startsWith("[Epic Report]")) {
-        require("./commands/epicreport.js")(message);
-        return;
-    }
+  //Test du suffixe
+  if (!message.content.startsWith(prefixCmd)) {
+    require('./commands/suffix.js')(message);
+    return;
+  }
 
-    //Test du suffixe
-    if (!message.content.startsWith(prefixCmd)) {
-        require("./commands/suffix.js")(message);
-        return;
-    }
+  //Récupération des paramètres envoyés
+  const args = message.content.slice(prefixCmd.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
 
-    //Récupération des paramètres envoyés
-    const args = message.content.slice(prefixCmd.length).trim().split(/ +/);
-    const command = args.shift().toLowerCase();
-
-    //Commandes
-    switch (command) {
-        case "choupiz":
-            require("./commands/choupiz.js")(message, args, client);
-            break;
-        case "mission":
-            require("./commands/mission.js")(message);
-            break;
-        case "help":
-            require("./commands/help.js")(message, prefixCmd);
-            break;
-        case "g":
-        case "s":
-            require("./commands/move.js")(message, command, args, client);
-            break;
-        case "fusee":
-            require("./commands/fusee.js")(message);
-            break;
-        case "hello":
-            require("./commands/hello.js")(message);
-            break;
-        case "culture":
-            require("./commands/culture.js")(message);
-            break;
-        case "efx":
-            require("./commands/efx.js")(message);
-            break;
-        case "bunny":
-            require("./commands/bunny.js")(message);
-            break;
-        case "quoi":
-            require("./commands/quoi.js")(message);
-            break;
-        case "hug":
-            require("./commands/hug.js")(message, args, client);
-            break;
-        case "punch":
-            require("./commands/punch.js")(message, args, client);
-            break;
-        case "hat":
-            require("./commands/hat.js")(message, args, client);
-            break;
-        case "stat":
-            require("./commands/stat.js")(message, args, client);
-            break;
-        default:
-            break;
-    };
+  //Commandes
+  switch (command) {
+    case 'choupiz':
+      require('./commands/choupiz.js')(message, args, client);
+      break;
+    case 'mission':
+      require('./commands/mission.js')(message);
+      break;
+    case 'help':
+      require('./commands/help.js')(message, prefixCmd);
+      break;
+    case 'g':
+    case 's':
+      require('./commands/move.js')(message, command, args, client);
+      break;
+    case 'fusee':
+      require('./commands/fusee.js')(message);
+      break;
+    case 'hello':
+      require('./commands/hello.js')(message);
+      break;
+    case 'culture':
+      require('./commands/culture.js')(message);
+      break;
+    case 'efx':
+      require('./commands/efx.js')(message);
+      break;
+    case 'bunny':
+      require('./commands/bunny.js')(message);
+      break;
+    case 'quoi':
+      require('./commands/quoi.js')(message);
+      break;
+    case 'hug':
+      require('./commands/hug.js')(message, args, client);
+      break;
+    case 'punch':
+      require('./commands/punch.js')(message, args, client);
+      break;
+    case 'hat':
+      require('./commands/hat.js')(message, args, client);
+      break;
+    case 'stat':
+      require('./commands/stat.js')(message, args, client);
+      break;
+    case 'c':
+      require('./commands/cleverbot.js')(message, args, client);
+      break;
+    default:
+      break;
+  }
 });
 
-
 client.login(process.env.BOT_TOKEN);
+
